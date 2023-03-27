75e51c0fa8a19b9e472b07df045d8f05a8cc1a9f
Zibusiso <znmangoye@gmail.com>
2023-03-27 18:41:13 +0200

    started work on the pipeline

--- gold-news-trends/src/gold_news_trends/pipeline_registry.py
+++ gold-news-trends/src/gold_news_trends/pipeline_registry.py
@@ -12,5 +12,5 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     pipelines = find_pipelines()
     pipelines["__default__"] = sum(pipelines.values())
-    
+    pipelines["market_data"] = pipeline(pipelines["market_data"], namespace="market_data")
     return pipelines
