b71edffbe2c998ec8a4a751599906f208245ed7e
Zibusiso <znmangoye@gmail.com>
2023-04-06 16:46:11 +0200

    working on nodes

--- gold-news-trends/src/gold_news_trends/pipelines/Market_Data/README.md
+++ gold-news-trends/src/gold_news_trends/pipelines/Market_Data/README.md
@@ -1,21 +1,17 @@
 # Pipeline market_data
 
-> *Note:* This is a `README.md` boilerplate generated using `Kedro 0.18.6`.
-
 ## Overview
 
-<!---
-Please describe your modular pipeline here.
--->
-
+This pipeline process market data and stores the data into a database
 ## Pipeline inputs
 
 <!---
 The list of pipeline inputs.
 -->
-
+OHLC data sourced from twelvedata api. 
 ## Pipeline outputs
 
 <!---
 The list of pipeline outputs.
 -->
+OHLC data stored in a postgres database. 
--- gold-news-trends/src/gold_news_trends/pipelines/Market_Data/nodes.py
+++ gold-news-trends/src/gold_news_trends/pipelines/Market_Data/nodes.py
@@ -1,27 +1,80 @@
-def __get(url, params):
-    import requests
-    resp = requests.get(url=url, params=params)
+import requests
+import random
+import time
+from bs4 import BeautifulSoup
+
+def test_proxy(proxy):
+    try:
+        response = requests.get("https://www.reuters.com", proxies={"http": proxy, "https": proxy}, timeout=5)
+        return response.status_code == 200
+    except:
+        return False
+
+def select_proxy(proxy_list):
+    # Randomly select a proxy from the list and test it
+    proxy = random.choice(proxy_list)
+    while not test_proxy(proxy):
+        print(f"Proxy {proxy} not working. Removing from list...")
+        proxy_list.remove(proxy)
+        proxy = random.choice(proxy_list)
+        
+def __get(url):
+    
+    # Define the proxies and headers
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    
+    # get list from file 
+    proxy_list = [
+        "http://proxy1.example.com:1234",
+        "http://proxy2.example.com:5678",
+        "http://proxy3.example.com:9101"
+    ]
+    
+    proxy = select_proxy(proxy_list)
+    # Make the request using the selected proxy and headers
+    proxies = {"http": proxy, "https": proxy}
+    resp = requests.get(url=url, headers=headers, proxies=proxies)
     if resp.status_code != 200:
         raise Exception(f"Failed to download url {url}.")
-    return resp.content
+    return resp
+
+def process_article(article_link):
+    response = __get(article_link)
+    with open(response.content) as fs:
+        article_soup = BeautifulSoup(fs, 'lxml')
+         
+        article_title = article_soup.find("h1").text.strip()
+        date = article_soup.find("time").text
+        time = article_soup.find("time").next_sibling.text
+        body = article_soup.find("pre").text
+        
+        return [date,time,article_title,article_link,body]
+    
 
 def get_news_data():
-    params = {}
+    
+    df = pd.DataFrame(columns=["article_date","article_time",
+                               "article_title","article_link","article_body"])
+
     for i in range(605):
         page_url = f"https://www.reuters.com/news/archive/goldMktRpt?page={i}"
-        response = __get(page_url, params)
+        response = __get(page_url)
         
-        # In response look for div class="news-headline-list  "
-        # Then get all a tags in the list and create urls stored in a list
-        # visit each url and get content
-    article_links = []
-    response = __get(url, params)
+        with open(response.content) as fp:
+            articles_soup = BeautifulSoup(fp, 'lxml')
+            articles =  articles_soup.find_all("article")
+            
+            for article in articles:
+                article_link = article.find("a", href=True).attrs["href"].strip()
+                df.loc[len(df)] = process_article(article_link)        
+            
+            df.to_csv("gold_articles.csv", mode="a", index=False, header=False)
+            
+            sleep_time = random.uniform(1, 5)
+            print(f"Scraped {i} pages")
+            print(f"Sleeping for {sleep_time:.2f} seconds before next request...")
+            time.sleep(sleep_time)
+            
     
-    # scrape an article and wait 10 sec before scraping next article
-    # (10*10*604)
-    
-    # goto page
-    # get list of article links 
-    # visit each link and get article info 
-    # Store article info in dict
-    # export to csv
